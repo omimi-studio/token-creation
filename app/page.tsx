10aa8f6ee6fe0d80cdac62810f1b53273704360c
Baranabas Oretan <barnabas.oretan@gmail.com>
2023-07-15 07:54:22 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Home } from "./home/page";
+
+export default function index() {
+  return (
+    <div className="mx-auto max-w-7xl px-6 sm:px-8 py-20 ">
+      <Home />
+    </div>
+  );
+}
